e0b243e38be350e560641b7c1dd3ca66c4ee1b73
Bruno <112035641+BrunoCode69@users.noreply.github.com>
2023-04-07 18:48:57 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-url_download_files="https://github.com/BrunoCode69/Utilitarios/blob/main/whatsapp_bot.zip";
+url_download_files="";
 branco="\033[1;37m";
 azul="\033[0;34m";
 red="\033[0;31m";
@@ -8,13 +8,13 @@ amarelo="\033[1;33m";
 barra="\E[44;1;37m";
 tag_barra="\E[0m";
 tag="\033[0m";
-
 ############### DEV: @Bruno_VPN ###############
 clear;
 apt-get update -y && apt-get upgrade -y
 apt install unzip at jq curl -y
 mkdir whatsapp_bot && cd whatsapp_bot;
-wget $url_download_files && unzip whatsapp_bot.zip;
+wget https://github.com/BrunoCode69/Utilitarios/blob/main/whatsapp_bot.zip;
+unzip whatsapp_bot.zip;
 rm -r whatsapp_bot.zip;
 chmod 777 $(ls);
 echo -e "${azul}SINCRONIZADO COM SUCESSO !${tag}";
